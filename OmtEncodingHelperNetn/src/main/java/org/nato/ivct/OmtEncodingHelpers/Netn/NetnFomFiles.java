2ec8c187a3c6c2fd99c19c74eaad7ea95a741b3e
Reinhard Herzog <reinhard.herzog@iosb.fraunhofer.de>
2025-02-20 14:54:56 +0100

    tmp file location

--- OmtEncodingHelperNetn/src/main/java/org/nato/ivct/OmtEncodingHelpers/Netn/NetnFomFiles.java
+++ OmtEncodingHelperNetn/src/main/java/org/nato/ivct/OmtEncodingHelpers/Netn/NetnFomFiles.java
@@ -1,41 +1,43 @@
 package org.nato.ivct.OmtEncodingHelpers.Netn;
 
+import java.io.IOException;
+
 import org.nato.ivct.OmtEncodingHelpers.RPR.RprFomFiles;
 
 public class NetnFomFiles extends RprFomFiles {
 
-    protected static final String FOM_PATH = "/NETN-FOM-4.0/";
+    protected static final String FOM_PATH = "NETN-FOM-4.0/";
 
     public NetnFomFiles() {
         super();
     }
 
     public NetnFomFiles addNetnBase() {
-        fomList.add(NetnFomFiles.class.getResource(FOM_PATH + "NETN-BASE.xml"));
+        fomList.add(NetnFomFiles.class.getResource("/" + FOM_PATH + "NETN-BASE.xml"));
         return this;
     }
 
-    public NetnFomFiles addTmpNetnBase() {
+    public NetnFomFiles addTmpNetnBase() throws IOException {
         fomList.add(createTempFile(FOM_PATH + "NETN-BASE", ".xml"));
         return this;
     }
 
     public NetnFomFiles addNetnSmc() {
-        fomList.add(NetnFomFiles.class.getResource(FOM_PATH + "NETN-SMC.xml"));
+        fomList.add(NetnFomFiles.class.getResource("/" + FOM_PATH + "NETN-SMC.xml"));
         return this;
     }
     
-    public NetnFomFiles addTmpNetnSmc() {
+    public NetnFomFiles addTmpNetnSmc() throws IOException {
         fomList.add(createTempFile(FOM_PATH + "NETN-SMC", ".xml"));
         return this;
     }
     
     public NetnFomFiles addNetnEtr() {
-        fomList.add(NetnFomFiles.class.getResource(FOM_PATH + "NETN-ETR.xml"));
+        fomList.add(NetnFomFiles.class.getResource("/" + FOM_PATH + "NETN-ETR.xml"));
         return this;
     }
     
-    public NetnFomFiles addTmpNetnEtr() {
+    public NetnFomFiles addTmpNetnEtr() throws IOException {
         fomList.add(createTempFile(FOM_PATH + "NETN-ETR", ".xml"));
         return this;
     }
