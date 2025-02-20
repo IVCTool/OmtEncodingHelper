ef23a0a44a5ff4d53855beee0143ab6d081ae85a
Reinhard Herzog <reinhard.herzog@iosb.fraunhofer.de>
2025-02-20 12:17:04 +0100

    initial code

--- OmtEncodingHelperNetn/src/main/java/org/nato/ivct/OmtEncodingHelpers/Netn/Base/datatypes/ArrayOfUuidStruct.java
+++ OmtEncodingHelperNetn/src/main/java/org/nato/ivct/OmtEncodingHelpers/Netn/Base/datatypes/ArrayOfUuidStruct.java
@@ -1,5 +1,98 @@
 package org.nato.ivct.OmtEncodingHelpers.Netn.Base.datatypes;
 
-public class ArrayOfUuidStruct {
+import java.util.Iterator;
 
+import hla.rti1516e.exceptions.RTIexception;
+import hla.rti1516e.exceptions.RTIinternalError;
+import hla.rti1516e.RtiFactoryFactory;
+import hla.rti1516e.encoding.ByteWrapper;
+import hla.rti1516e.encoding.DataElementFactory;
+import hla.rti1516e.encoding.DecoderException;
+import hla.rti1516e.encoding.EncoderException;
+import hla.rti1516e.encoding.HLAvariableArray;
+
+/**
+ * An array of Unique Identifiers expressed as UUIDs.
+ * 
+ * <dataType>UUID</dataType>
+ * <cardinality>Dynamic</cardinality>
+ * <encoding>HLAvariableArray</encoding>
+ * <semantics></semantics>
+ */
+public class ArrayOfUuidStruct implements HLAvariableArray<UUIDStruct> {
+
+    private HLAvariableArray<UUIDStruct> value;
+
+    static DataElementFactory<UUIDStruct> byteFactory = new DataElementFactory<UUIDStruct>()
+    {
+        public UUIDStruct createElement(int index) 
+        {
+            try {
+                return new UUIDStruct();
+            } catch (RTIexception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }            
+    };
+
+    public ArrayOfUuidStruct() throws RTIinternalError {
+        value = RtiFactoryFactory.getRtiFactory().getEncoderFactory().createHLAvariableArray(byteFactory);
+    }
+    
+    @Override
+    public int getOctetBoundary() {
+        return value.getOctetBoundary();
+    }
+
+    @Override
+    public int getEncodedLength() {
+        return value.getEncodedLength();
+    }
+
+    @Override
+    public byte[] toByteArray() {
+        return value.toByteArray();
+    }
+
+    @Override
+    public void encode(ByteWrapper byteWrapper) throws EncoderException {
+        value.encode(byteWrapper);
+    }
+
+    @Override
+    public void decode(ByteWrapper byteWrapper) throws DecoderException {
+        value.decode(byteWrapper);
+    }
+
+    @Override
+    public void decode(byte[] bytes) throws DecoderException {
+        value.decode(bytes);
+    }
+
+    @Override
+    public void addElement(UUIDStruct dataElement) {
+        value.addElement(dataElement);
+    }
+
+    @Override
+    public int size() {
+        return value.size();
+    }
+
+    @Override
+    public UUIDStruct get(int index) {
+        return value.get(index);
+    }
+
+    @Override
+    public Iterator<UUIDStruct> iterator() {
+        return value.iterator();
+    }
+
+    @Override
+    public void resize(int newSize) {
+        value.resize(newSize);
+    }
+    
 }
