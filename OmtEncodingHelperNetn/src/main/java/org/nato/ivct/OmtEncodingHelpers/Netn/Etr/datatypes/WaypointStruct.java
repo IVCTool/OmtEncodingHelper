81a835031e424d7ba5a4519b6d6795dd338f1cd7
Dirk Mühlenberg <dirk.muehlenberg@iosb.fraunhofer.de>
2025-04-01 15:56:21 +0200

    added TaskDefinitionVarianStruct for MoveByRoute, MoveToLocation

---
+++ OmtEncodingHelperNetn/src/main/java/org/nato/ivct/OmtEncodingHelpers/Netn/Etr/datatypes/WaypointStruct.java
@@ -0,0 +1,64 @@
+package org.nato.ivct.OmtEncodingHelpers.Netn.Etr.datatypes;
+
+import org.nato.ivct.OmtEncodingHelpers.Core.datatypes.HLAfixedRecordStruct;
+import org.nato.ivct.OmtEncodingHelpers.Netn.Base.datatypes.LocationStruct;
+
+import hla.rti1516e.encoding.HLAfloat32BE;
+import hla.rti1516e.exceptions.RTIinternalError;
+
+/*
+        <fixedRecordData>
+            <name>Waypoint</name>
+            <encoding>HLAfixedRecord</encoding>
+            <semantics>A location and the speed to reach that location.</semantics>
+            <field>
+                <name>Location</name>
+                <dataType>LocationStruct</dataType>
+                <semantics>Coordinate to move towards.</semantics>
+            </field>
+            <field>
+                <name>Speed</name>
+                <dataType>VelocityMeterPerSecondFloat32</dataType>
+                <semantics>Speed over ground for an entity, in m/s.</semantics>
+            </field>
+            <field>
+                <name>SegmentMaxWidth</name>
+                <dataType>MeterFloat32</dataType>
+                <semantics>Maximum allowed width on road or terrain. Use zero (0) for no max width.</semantics>
+            </field>
+        </fixedRecordData>
+ */
+public class WaypointStruct extends HLAfixedRecordStruct {
+
+    public enum AttributeName {
+        Location, Speed, SegmentMaxWidth
+    }
+
+    public WaypointStruct() throws RTIinternalError {
+        super();
+        add(AttributeName.Location.name(), new LocationStruct());
+        add(AttributeName.Speed.name(), encoderFactory.createHLAfloat32BE());
+        add(AttributeName.SegmentMaxWidth.name(), encoderFactory.createHLAfloat32BE());
+    }
+
+    public LocationStruct getLocation() {
+        return (LocationStruct)get(AttributeName.Location.name());
+    }
+    public void setLocation(LocationStruct loaction) {
+        set(AttributeName.Location.name(), loaction);
+    }
+
+    public HLAfloat32BE getSpeed() {
+        return (HLAfloat32BE)get(AttributeName.Speed.name());
+    }
+    public void setSpeed(HLAfloat32BE speed) {
+        set(AttributeName.Speed.name(), speed);
+    }
+
+    public HLAfloat32BE getSegmentMaxWidth() {
+        return (HLAfloat32BE)get(AttributeName.SegmentMaxWidth.name());
+    }
+    public void setSegmentMaxWidth(HLAfloat32BE segmentMaxWidth) {
+        set(AttributeName.SegmentMaxWidth.name(), segmentMaxWidth);
+    }
+}
