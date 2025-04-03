0951543a9d268f998ad62bb2aade421836a472ec
Dirk Mühlenberg <dirk.muehlenberg@iosb.fraunhofer.de>
2025-04-03 08:13:46 +0200

    renamed createTime to sendTime

--- OmtEncodingHelperNetn/src/main/java/org/nato/ivct/OmtEncodingHelpers/Netn/Base/interactions/HLAinteractionRoot.java
+++ OmtEncodingHelperNetn/src/main/java/org/nato/ivct/OmtEncodingHelpers/Netn/Base/interactions/HLAinteractionRoot.java
@@ -4,7 +4,6 @@ import org.nato.ivct.OmtEncodingHelpers.Core.OmtEncodingHelperException;
 import org.nato.ivct.OmtEncodingHelpers.Netn.Base.datatypes.EpochTimeStruct;
 import org.nato.ivct.OmtEncodingHelpers.Netn.Base.datatypes.UUIDStruct;
 
-import hla.rti1516e.encoding.DataElement;
 import hla.rti1516e.encoding.DataElementFactory;
 import hla.rti1516e.encoding.HLAbyte;
 import hla.rti1516e.exceptions.FederateNotExecutionMember;
@@ -16,23 +15,23 @@ import hla.rti1516e.exceptions.RTIinternalError;
 public class HLAinteractionRoot extends org.nato.ivct.OmtEncodingHelpers.Core.interactions.HLAinteractionRoot {
 
     public enum AttributeName {
-        CreateTime,
+        SendTime,
         UniqueId
     }
 
     public HLAinteractionRoot() throws NameNotFound, FederateNotExecutionMember, NotConnected, RTIinternalError, OmtEncodingHelperException, hla.rti1516e.exceptions.NameNotFound, hla.rti1516e.exceptions.FederateNotExecutionMember {
         super();
-        addParameter(AttributeName.CreateTime.name(), encoderFactory.createHLAinteger32BE());
+        addParameter(AttributeName.SendTime.name(), encoderFactory.createHLAinteger32BE());
         addParameter(AttributeName.UniqueId.name(), (UUIDStruct) encoderFactory.createHLAfixedArray((DataElementFactory<HLAbyte>) encoderFactory, 16));
     }
 
-    public void setCreateTime (int createTime) throws NameNotFound, FederateNotExecutionMember, NotConnected, RTIinternalError {
-        EpochTimeStruct value = (EpochTimeStruct) getParameter(AttributeName.CreateTime.name());
-        value.setValue(createTime);
-        setParameter(AttributeName.CreateTime.name(), value.toByteArray());
+    public void setSendTime (int SendTime) throws NameNotFound, FederateNotExecutionMember, NotConnected, RTIinternalError {
+        EpochTimeStruct value = (EpochTimeStruct) getParameter(AttributeName.SendTime.name());
+        value.setValue(SendTime);
+        setParameter(AttributeName.SendTime.name(), value.toByteArray());
     }
-    public int getCreateTime () throws NameNotFound, FederateNotExecutionMember, NotConnected, RTIinternalError {
-        EpochTimeStruct value = (EpochTimeStruct) getParameter(AttributeName.CreateTime.name());
+    public int getSendTime () throws NameNotFound, FederateNotExecutionMember, NotConnected, RTIinternalError {
+        EpochTimeStruct value = (EpochTimeStruct) getParameter(AttributeName.SendTime.name());
         return value.getValue();
     }
 }
