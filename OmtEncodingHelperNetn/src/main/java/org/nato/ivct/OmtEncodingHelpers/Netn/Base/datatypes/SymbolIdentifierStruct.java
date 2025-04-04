02a27c05456217029aed2fbcb5a4a589b6303f4c
Dirk Mühlenberg <dirk.muehlenberg@iosb.fraunhofer.de>
2025-04-04 14:45:20 +0200

    added TrackStruct object

---
+++ OmtEncodingHelperNetn/src/main/java/org/nato/ivct/OmtEncodingHelpers/Netn/Base/datatypes/SymbolIdentifierStruct.java
@@ -0,0 +1,14 @@
+package org.nato.ivct.OmtEncodingHelpers.Netn.Base.datatypes;
+
+import org.nato.ivct.OmtEncodingHelpers.Core.datatypes.HLAvariableArrayStruct;
+
+import hla.rti1516e.encoding.HLAunicodeChar;
+import hla.rti1516e.exceptions.RTIinternalError;
+
+public class SymbolIdentifierStruct extends HLAvariableArrayStruct<HLAunicodeChar> {
+
+    public SymbolIdentifierStruct() throws RTIinternalError {
+        super();
+    }
+
+}
---
+++ OmtEncodingHelperNetn/src/main/java/org/nato/ivct/OmtEncodingHelpers/Netn/Etr/datatypes/ArrayOfDetectedEquipmentStruct.java
@@ -0,0 +1,13 @@
+package org.nato.ivct.OmtEncodingHelpers.Netn.Etr.datatypes;
+
+import org.nato.ivct.OmtEncodingHelpers.Core.datatypes.HLAvariableArrayStruct;
+
+import hla.rti1516e.exceptions.RTIinternalError;
+
+public class ArrayOfDetectedEquipmentStruct extends HLAvariableArrayStruct<DetectedEquipmentStruct> {
+
+    public ArrayOfDetectedEquipmentStruct() throws RTIinternalError {
+        super();
+    }
+
+}
